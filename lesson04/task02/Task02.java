f83c2aa6d767104744e1c84aca0fc84b18e78491
Maksim <angry.school.boy@mail.ru>
2019-05-29 01:18:23 +0300

    commit

--- lesson04/task02/Task02.java
+++ lesson04/task02/Task02.java
@@ -8,7 +8,7 @@ public class Task02 {
 		double ringSquare = RingLogic.findArea(r1, r2);
 		
 		Printer.print("The area of the ring with an outer" 
-				+ " radius of" + "\n" + r1 + " and an inner"
-				+ " radius of " + r2 + " is equal to " + ringSquare + "\n");
+			+ " radius of" + "\n" + r1 + " and an inner"
+			+ " radius of " + r2 + " is equal to " + ringSquare + "\n");
 	}
 }
