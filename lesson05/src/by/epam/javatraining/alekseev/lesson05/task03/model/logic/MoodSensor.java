89bc694c9e673bbeaeb101e1daea75585d404290
Oddy-Nuff-Da-Snow-Leopard <43066116+Oddy-Nuff-Da-Snow-Leopard@users.noreply.github.com>
2019-06-03 23:29:08 +0300

    Update MoodSensor.java

--- lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/MoodSensor.java
+++ lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/MoodSensor.java
@@ -2,35 +2,30 @@ package by.epam.javatraining.alekseev.lesson05.task03.model.logic;
 
 public class MoodSensor {
 
-    private static final int BAD_MOOD_LIMIT = 2;
-    private static final int SAD_MOOD_LIMIT = 4;
-    private static final int GOOD_MOOD_LIMIT = 6;
-    private static final int GREAT_MOOD_LIMIT = 8;
+    private static final int BAD_MOOD_SMILEY_NUMBER = 0;
+    private static final int SAD_MOOD_SMILEY_NUMBER = 1;
+    private static final int GOOD_MOOD_SMILEY_NUMBER = 2;
+    private static final int GREAT_MOOD_SMILEY_NUMBER = 3;
+    private static final int EXCELLENT_MOOD_SMILEY_NUMBER = 4;
+    
+    private static final String BAD_MOOD_SMILEY = ";\";
+    private static final String SAD_MOOD_SMILEY = ":(";
+    private static final String GOOD_MOOD_SMILEY = ":)";
+    private static final String GREAT_MOOD_SMILEY = ":)))";
+    private static final String EXCELLENT_MOOD_SMILEY = ":D";
 
     public static String getRandomMood() {
         int randMoodNumber = Random.getRandomNumber();
         return getMood(randMoodNumber);
     }
 
-    public static String getMood(int moodNumber) {
+    private static String getMood(int moodNumber) {
 
-        String mood = null;
+        String mood = EXCELLENT_MOOD_SMILEY;
 
-        if (moodNumber <= BAD_MOOD_LIMIT) {
-            mood = ":|";
-        } else if (moodNumber > BAD_MOOD_LIMIT
-                && moodNumber <= SAD_MOOD_LIMIT) {
-            mood = ":(";
-        } else if (moodNumber > SAD_MOOD_LIMIT
-                && moodNumber <= GOOD_MOOD_LIMIT) {
-            mood = ";)";
-        } else if (moodNumber > GOOD_MOOD_LIMIT
-                && moodNumber <= GREAT_MOOD_LIMIT) {
-            mood = ":)))";
-        } else if (moodNumber > GREAT_MOOD_LIMIT) {
-            mood = ":D";
-            return mood;
-        }
+        switch(moodNumber)
+                
+        
         return mood;
     }
 }
