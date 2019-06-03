ae39f16935952e0288d4f1481e3c623b488561e1
Oddy-Nuff-Da-Snow-Leopard <43066116+Oddy-Nuff-Da-Snow-Leopard@users.noreply.github.com>
2019-06-03 23:57:56 +0300

    Update MoodSensor.java

--- lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/MoodSensor.java
+++ lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/MoodSensor.java
@@ -6,9 +6,8 @@ public class MoodSensor {
     private static final int SAD_MOOD_SMILEY_NUMBER = 1;
     private static final int GOOD_MOOD_SMILEY_NUMBER = 2;
     private static final int GREAT_MOOD_SMILEY_NUMBER = 3;
-    private static final int EXCELLENT_MOOD_SMILEY_NUMBER = 4;
     
-    private static final String BAD_MOOD_SMILEY = ";\";
+    private static final String BAD_MOOD_SMILEY = ";\\";
     private static final String SAD_MOOD_SMILEY = ":(";
     private static final String GOOD_MOOD_SMILEY = ":)";
     private static final String GREAT_MOOD_SMILEY = ":)))";
@@ -18,14 +17,22 @@ public class MoodSensor {
         int randMoodNumber = Random.getRandomNumber();
         return getMood(randMoodNumber);
     }
+    
+    public static String wrapMethod(int moodNumber){
+        return getMood(moodNumber);
+    }
 
     private static String getMood(int moodNumber) {
 
         String mood = EXCELLENT_MOOD_SMILEY;
 
-        switch(moodNumber)
-                
-        
+        switch(moodNumber){
+            case BAD_MOOD_SMILEY_NUMBER: mood = BAD_MOOD_SMILEY; break;
+            case SAD_MOOD_SMILEY_NUMBER: mood = SAD_MOOD_SMILEY; break;
+            case GOOD_MOOD_SMILEY_NUMBER: mood = GOOD_MOOD_SMILEY; break;
+            case GREAT_MOOD_SMILEY_NUMBER: mood = GREAT_MOOD_SMILEY; break;
+        }
         return mood;
     }
 }
+
