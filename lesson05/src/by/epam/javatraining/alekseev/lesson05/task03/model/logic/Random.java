b1a7b61b911de72fba82006dc7eb70bb7921c3be
Oddy-Nuff-Da-Snow-Leopard <43066116+Oddy-Nuff-Da-Snow-Leopard@users.noreply.github.com>
2019-06-05 13:44:03 +0300

    Update Random.java

--- lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/Random.java
+++ lesson05/src/by/epam/javatraining/alekseev/lesson05/task03/model/logic/Random.java
@@ -2,7 +2,7 @@ package by.epam.javatraining.alekseev.lesson05.task03.model.logic;
 
 public class Random {
 
-    private static final int MAX_VALUE = 4;
+    private static final int MAX_VALUE = 5;
     private static final java.util.Random RND = new java.util.Random();
 
     public static int getRandomNumber() {
